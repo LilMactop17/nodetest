21100822d833631162994dafcc6a93131c17536d
Mohamed Tawfik Omar <mooniomar17@gmail.com>
2025-03-25 23:36:05 -0400

    Changed to console log

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const express = require("express");
-const app = express();
-
-app.get('users/test', (req, res) => {
-    res.send({ msg: 'Testing'});
-});
-
-app.listen(5000, () => console.log('Running on port 5000'));
+console.log("Hello");
